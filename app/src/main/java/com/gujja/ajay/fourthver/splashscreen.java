09ddc41a20f6a16a7a596b9e6a30400cd4d35a13
ajaygujja <ajaygujja5@gmail.com>
2020-02-22 15:35:59 +0530

    Transition Splash Screen Material color Added

--- app/src/main/java/com/gujja/ajay/fourthver/splashscreen.java
+++ app/src/main/java/com/gujja/ajay/fourthver/splashscreen.java
@@ -6,15 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class splashscreen extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2000;
+    ImageView splash_img;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splashscreen);
+
+        splash_img = findViewById(R.id.splash_image);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransistion);
+
+        splash_img.setAnimation(myanim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
